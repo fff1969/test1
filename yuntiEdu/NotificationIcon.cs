ed7315e40cafad59bcb6b9bb9ceee94f55eb01f9
fff1969 <fff1969@163.com>
2014-01-24 22:23:17 +0800

    0.1
    001

---
+++ yuntiEdu/NotificationIcon.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： john
+ * 日期: 2014/1/24
+ * 时间: 20:15
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace yuntiEdu
+{
+	public sealed class NotificationIcon
+	{
+		private NotifyIcon notifyIcon;
+		private ContextMenu notificationMenu;
+		
+		#region Initialize icon and menu
+		public NotificationIcon()
+		{
+			notifyIcon = new NotifyIcon();
+			notificationMenu = new ContextMenu(InitializeMenu());
+			
+			notifyIcon.DoubleClick += IconDoubleClick;
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotificationIcon));
+			notifyIcon.Icon = (Icon)resources.GetObject("$this.Icon");
+			notifyIcon.ContextMenu = notificationMenu;
+		}
+		/// <summary>
+		/// 快捷菜单
+		/// </summary>
+		/// <returns></returns>
+		private MenuItem[] InitializeMenu()
+		{
+			MenuItem[] menu = new MenuItem[] {
+				new MenuItem("关于", menuAboutClick),
+				new MenuItem("退出", menuExitClick)
+			};
+			return menu;
+		}
+		#endregion
+		
+		#region Main - Program entry point
+		/// <summary>Program entry point.</summary>
+		/// <param name="args">Command Line Arguments</param>
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			
+			bool isFirstInstance;
+			// Please use a unique name for the mutex to prevent conflicts with other programs
+			using (Mutex mtx = new Mutex(true, "yuntiEdu", out isFirstInstance)) {
+				if (isFirstInstance) {
+					NotificationIcon notificationIcon = new NotificationIcon();
+					notificationIcon.notifyIcon.Visible = true;
+					Application.Run();
+					notificationIcon.notifyIcon.Dispose();
+				} else {
+					// The application is already running
+					// TODO: Display message box or change focus to existing application instance
+				}
+			} // releases the Mutex
+		}
+		#endregion
+		
+		#region Event Handlers
+		private void menuAboutClick(object sender, EventArgs e)
+		{
+			MessageBox.Show("About This Application");
+		}
+		
+		private void menuExitClick(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+		
+		private void IconDoubleClick(object sender, EventArgs e)
+		{
+			MessageBox.Show("The icon was double clicked");
+		}
+		#endregion
+	}
+}
---
+++ yuntiEdu/scratchDuino.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： john
+ * 日期: 2014/1/24
+ * 时间: 21:53
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+
+namespace yuntiEdu
+{
+	/// <summary>
+	/// Description of scratchDuino.
+	/// </summary>
+	public class scratchDuino
+	{
+		public scratchDuino()
+		{
+		}
+	}
+}
